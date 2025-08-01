a53630dc50ec92fd5f68c969abdf1fb4088f13a1
Habtamu Tesfaye <habtamutesfaye678@gmail.com>
2025-08-01 15:29:11 +0300

    feat: added travel app with CRUD

---
+++ alx_travel_app/listings/views.py
@@ -0,0 +1,27 @@
+from rest_framework import viewsets, permissions
+from .models import Listing, Booking, Review
+from .serializers import ListingSerializer, BookingSerializer, ReviewSerializer
+
+class ListingViewSet(viewsets.ModelViewSet):
+    queryset = Listing.objects.all()
+    serializer_class = ListingSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(host=self.request.user)
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(guest=self.request.user)
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(reviewer=self.request.user)
